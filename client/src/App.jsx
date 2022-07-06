e5399d3974dbb2093173c26efd536db8bc88e4dd
oaat9309 <kite1993@naver.com>
2022-07-06 15:59:55 +0900

    feat: Added basic Routing for Login 로그인을 위한 기본 라우팅 설정을 완료했습니다.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
 import MainPage from "./pages/MainPage";
+import SiginIn from "./pages/SiginIn";
+
 import { CustomContainer } from "./styles/containerStyle";
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/main" element={<MainPage />} />
+            <Route path="/login" element={<SiginIn />} />
             {/* 로그인 된 사람 중 키워드 있는 사람만  */}
             <Route path="*" element={<ErrorPage />} />
           </Routes>
