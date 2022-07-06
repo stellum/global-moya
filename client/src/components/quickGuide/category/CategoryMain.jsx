cee2ac012eb0c619d59272faaa77b451e6f18576
sacultang <sacultang@gmail.com>
2022-07-06 16:33:52 +0900

    test

--- client/src/components/quickGuide/category/CategoryMain.jsx
+++ client/src/components/quickGuide/category/CategoryMain.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import QuickGuideHeader from "../QuickGuideHeader";
-import { DefaultContainer } from "@styles/containerStyle";
 import CategoryButton from "./CategoryButton";
+import LvKeywordList from "./LvKeywordList";
+import { DefaultContainer } from "@styles/containerStyle";
+import { getCategoryList } from "../../../api/masterApi";
+import { useParams } from "react-router-dom";
 const CategoryMain = () => {
+  const params = useParams();
+  const category = params.id;
+  useEffect(() => {
+    const data = getCategoryList(category);
+  }, []);
   return (
     <DefaultContainer>
       <CategoryButton />
       <QuickGuideHeader />
+      <LvKeywordList data={data} />
     </DefaultContainer>
   );
 };
