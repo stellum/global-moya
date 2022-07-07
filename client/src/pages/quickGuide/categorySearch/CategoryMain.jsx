11ec6082fd30dbeb17a1ba3281424a1d0498d415
sacultang <sacultang@gmail.com>
2022-07-08 01:25:45 +0900

    feat : QuickGuide 카테고리 100개씩 분리

--- client/src/pages/quickGuide/categorySearch/CategoryMain.jsx
+++ client/src/pages/quickGuide/categorySearch/CategoryMain.jsx
@@ -1,73 +1,37 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 import QuickGuideHeader from "../QuickGuideHeader";
 import CategoryButton from "./CategoryButton";
+import LvKeywordList from "./LvKeywordList";
 // const LvKeywordList = React.lazy(() => import("./LvKeywordList"));
 
-import { DefaultContainer } from "@styles/common/container";
-import { useParams } from "react-router-dom";
-import LvKeywordList from "./LvKeywordList";
-import { useSelector } from "react-redux";
 import QuickCategoryHook from "../../../hooks/QuickCategoryHook";
-import { useQuery } from "react-query";
+import { DefaultContainer } from "@styles/common/container";
 import { getCategoryList } from "@api/masterApi";
+
 const CategoryMain = () => {
   const [dataList, setDataList] = useState([]);
   const [page, setPage] = useState(1);
-  const [totalResult, setTotalResult] = useState(0);
-  const [pageLength, setPageLength] = useState(0);
-
   const params = useParams();
-  const { data, isLoading } = useQuery("LvData", () => {
-    getCategoryList(params.id);
-  });
-  const storeMasterData = useSelector(
-    (state) => state.categorySlice.masterData
-  );
-  const storeLvData = useSelector((state) => state.categorySlice.lvCategory);
+  const category = params.id;
 
-  // console.log(storeMasterData[`${category}`]);
-  const { lastElementRef } = QuickCategoryHook(data, setPage, isLoading);
-
-  let minValue = 0;
-  let maxValue = 100;
+  const fetch = async () => {
+    const response = await getCategoryList(category);
+    const data = response.details;
+    setDataList(data);
+  };
+  const { lastElementRef } = QuickCategoryHook(setPage);
 
   useEffect(() => {
-    minValue = 0;
-    maxValue = 100;
+    fetch();
   }, []);
-
-  // 콘솔용
-  useEffect(() => {
-    console.log(storeLvData);
-    // console.log("페이지갯수", pageLength);
-    // console.log(storeMasterData.length);
-    // console.log("토탈", totalResult);
-    // console.log("page", page);
-
-    // if (page === 1) {
-    //   setDataList(storeLvData.slice(minValue, maxValue));
-    // } else {
-    //   setDataList((prev) => [
-    //     ...prev,
-    //     ...storeLvData.slice(minValue, maxValue),
-    //   ]); // 뒤로 누적
-    // }
-    minValue = maxValue;
-    maxValue += 100;
-    // console.log(minValue, maxValue);
-  }, [page]);
 
   return (
     <DefaultContainer>
       <CategoryButton />
       <QuickGuideHeader />
-      {/* <Suspense fallback={<div>loading...</div>}> */}
-      <LvKeywordList />
-      {/* </Suspense> */}
-      {/* {1 !== page && <div ref={target}></div>} */}
-      {/* {masterData.length > 0 && page > 1 ? (
-        <div ref={lastElementRef}>로딩중...</div>
-      ) : null} */}
+      <LvKeywordList dataList={dataList} myRef={lastElementRef} page={page} />
     </DefaultContainer>
   );
 };
--- client/src/pages/quickGuide/categorySearch/LvKeywordList.jsx
+++ client/src/pages/quickGuide/categorySearch/LvKeywordList.jsx
@@ -1,15 +1,43 @@
 import React, { useState, useEffect } from "react";
+import { SearchIcon } from "@styles/svgIcon";
+import {
+  KeywordLi,
+  KeywordH4,
+} from "@styles/quickGuide/categorySearch/LvKeywordList";
+const LvKeywordList = ({ dataList, myRef, page }) => {
+  const [sliceValue, setSliceValue] = useState({ minValue: 0, maxValue: 100 });
 
-const LvKeywordList = () => {
-  // console.log(dataList);
+  useEffect(() => {
+    setSliceValue((prev) => ({
+      minValue: prev.maxValue,
+      maxValue: prev.maxValue + 100,
+    }));
+  }, [page]);
   return (
     <ul>
-      {/* {dataList &&
-        dataList.map((item) => (
-          <li key={item._id} style={{ marginBottom: "10px" }}>
-            {item.name}
-          </li>
-        ))} */}
+      {dataList &&
+        dataList
+          .slice(
+            0,
+            dataList.length > 100 ? sliceValue.maxValue : dataList.length
+          )
+          .map((item, idx) => {
+            if (item.length === idx + 1) {
+              return (
+                <KeywordLi ref={myRef} key={item._id}>
+                  <SearchIcon />
+                  <KeywordH4>{item.name}</KeywordH4>
+                </KeywordLi>
+              );
+            } else {
+              return (
+                <KeywordLi ref={myRef} key={item._id}>
+                  <SearchIcon />
+                  <KeywordH4>{item.name}</KeywordH4>
+                </KeywordLi>
+              );
+            }
+          })}
     </ul>
   );
 };
