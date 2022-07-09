593ce3f054633e131350de71e313df44393309c6
CHAYUNA <sarasata3131@gmail.com>
2022-07-10 04:07:16 +0900

    feat : hamberger 버튼 클릭시 사이트 네비show up

--- client/src/styles/main/mainPageHeader.js
+++ client/src/styles/main/mainPageHeader.js
@@ -8,3 +8,11 @@ export const MainHeader = styled(DefaultHeader)`
   height: 60px;
   position: relative;
 `;
+export const SideHeader = styled(DefaultHeader)`
+  margin: 0 16px;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  position: relative;
+`;
