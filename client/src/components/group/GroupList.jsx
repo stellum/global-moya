35224e740806c625145d7b77aa03a274b71aa986
youngcodej22 <youngcodej22@gmail.com>
2022-07-12 17:47:48 +0900

    feat: UPDATE GroupList
    connect GroupList to EditKeyword

--- client/src/components/group/GroupList.jsx
+++ client/src/components/group/GroupList.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useState, forwardRef } from "react";
 import styled from "styled-components";
 import Hamburger from "../common/Hamburger";
+import Checkbox from "../common/Checkbox";
 import { DeleteIcon } from "@styles/svgIcon";
-import { colors } from "@styles/theme";
-const GroupList = () => {
+import { colors, fontSize } from "@styles/theme";
+
+const GroupList = forwardRef((props, ref) => {
+  const { text } = props;
+  const { type } = props;
+  const [checked, setChecked] = useState(false);
+
+  const handleCheckbox = (e) => {
+    setChecked(e.target.checked);
+  };
+
+  const handleInput = (e) => {
+    console.log("e", e.target.value);
+  };
+
   return (
     <GroupWrap>
-      <IconWrap>
-        <DeleteIcon />
-      </IconWrap>
-      <GroupTextWrap>
-        <Text>비지니스 모델</Text>
+      {/* <DeleteIcon /> */}
+
+      {/* <CheckInput type="checkbox" id="deleteCheck" /> */}
+      {/* <CheckLabel htmlFor="deleteCheck" /> */}
+
+      <Checkbox />
+
+      <GroupTextWrap type={type}>
+        {type === "keyword" ? (
+          <TextKeyword>{text}</TextKeyword>
+        ) : (
+          <InputGroup
+            type="text"
+            placeholder="그룹명을 바꿔주세요."
+            defaultValue={text}
+            onChange={handleInput}
+          />
+        )}
       </GroupTextWrap>
-      <HamburgerWrap>
+      <HamburgerWrap ref={ref}>
         <Hamburger />
       </HamburgerWrap>
     </GroupWrap>
   );
-};
+});
 
 export default GroupList;
 
@@ -30,16 +57,28 @@ const GroupWrap = styled.div`
   align-items: center;
   position: relative;
 `;
-const IconWrap = styled.div`
-  width: 48px;
-  display: flex;
-  justify-content: center;
-  svg {
-    height: 20px;
-  }
-`;
+// const IconWrap = styled.div`
+//   width: 48px;
+//   display: flex;
+//   justify-content: center;
+//   svg {
+//     height: 20px;
+//   }
+//   cursor: pointer;
+// `;
 const GroupTextWrap = styled.div`
-  border: 1px solid ${colors.gray250};
+  ${(props) => {
+    if (props.type === "group") {
+      return `
+        border: 1px solid ${colors.gray250};    
+      `;
+    }
+    if (props.type === "keyword") {
+      return `
+        border: 0;
+      `;
+    }
+  }}
   padding: 7px 10px;
   border-radius: 4px;
 `;
@@ -49,5 +88,10 @@ const HamburgerWrap = styled.div`
   right: 0;
   display: flex;
   justify-content: center;
+  cursor: pointer;
 `;
-const Text = styled.h4``;
+const TextKeyword = styled.h4``;
+
+const InputGroup = styled.input`
+  font-size: ${fontSize.FontSize16};
+`;
