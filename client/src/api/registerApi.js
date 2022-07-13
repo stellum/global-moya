d83801a1d597f0c4a1aefa4eb4f6094b85849f41
oaat9309 <kite1993@naver.com>
2022-07-13 12:59:31 +0900

    fix: Edited registerApi (07.13)

--- client/src/api/registerApi.js
+++ client/src/api/registerApi.js
@@ -3,13 +3,13 @@ import clientServer from "./baseUrl";
 export const registerFunc = async (form) => {
   try {
     const res = await clientServer({
-      url: "/auth/login",
+      url: "/auth/register",
       method: "post",
       data: form,
     });
 
     if (res.status === 200) {
-      console.log("로그인 성공");
+      console.log("회원가입 성공");
       return res.status;
     }
   } catch (err) {
