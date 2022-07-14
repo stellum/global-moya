c8faffb8d4c568abaaaa94ed49264cf0f1639a03
youngcodej22 <youngcodej22@gmail.com>
2022-07-14 14:21:01 +0900

    fix: UPDATE Home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,3 @@
-
-import React from "react";
-import HomeContent from "./HomeContent";
-import HomeFooter from "./HomeFooter";
-import TokenCheck from "../../hoc/TokenCheck";
-const Home = () => {
-
 import React, { useEffect, useRef } from "react";
 import { getKeywords } from "@api/keywordListApi";
 import { useSelector, useDispatch } from "react-redux";
@@ -54,7 +47,6 @@ const Home = () => {
     }
   }, []);
 
-
   return (
     <>
       <HomeContent />
@@ -63,4 +55,4 @@ const Home = () => {
   );
 };
 
-export default TokenCheck(Home);
+export default Home;
