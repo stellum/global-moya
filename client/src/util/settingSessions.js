5f80f7fdd94b312984795e89ff1c0c45889202da
youngcodej22 <youngcodej22@gmail.com>
2022-07-14 12:50:54 +0900

    fix: UPDATE settingSession
    bug at find and startEnd method

--- client/src/util/settingSessions.js
+++ client/src/util/settingSessions.js
@@ -4,11 +4,30 @@ export const setCookie = (token) => {
   document.cookie = `accessToken=${token}; max-age=900`;
 };
 
+// export const getCookie = () => {
+//   const accessToken = document.cookie;
+//   accessToken
+//     .split("; ")
+//     .find((row) => row.startsWith("accessToken"))
+//     .split("=")[1];
+//   return accessToken;
+// };
+
 export const getCookie = () => {
-  const accessToken = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("accessToken"))
-    .split("=")[1];
+  let key, value;
+  let splitCookies = document.cookie.split(";");
+  let accessToken;
+
+  for (let i = 0; i < splitCookies.length; i++) {
+    key = splitCookies[i].substring(0, splitCookies[i].indexOf("="));
+    value = splitCookies[i].substring(splitCookies[i].indexOf("=") + 1);
+    // 앞과 뒤의 공백 제거하기
+    key = key.replace(/^\s+|\s+$/g, "");
+
+    if (key === "accessToken") {
+      accessToken = value;
+    }
+  }
   return accessToken;
 };
 
