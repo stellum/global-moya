3acf9913d7b76f95668d9a99646785e97127900d
youngcodej22 <youngcodej22@gmail.com>
2022-07-18 13:36:01 +0900

    feat: UPDATE store
    connect buttonSlice

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,15 +1,18 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import userSlice from "../reducer/user/userSlice";
 import { persistStore } from "redux-persist";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import logger from "redux-logger";
+import buttonSlice from "../reducer/buttonSlice";
 import modalSlice from "../reducer/modalSlice";
 import cardTypeSlice from "../reducer/cardTypeSlice";
 import categorySlice from "../reducer/categorySlice";
-
 import subsSlice from "../reducer/user/subsSlice";
-
 import keywordConnectedSlice from "../reducer/keywordConnectedSlice";
 import searchFilterSlice from "../reducer/searchFilterSlice";
 
@@ -17,16 +20,15 @@ import searchFilterSlice from "../reducer/searchFilterSlice";
 //   middleware.push(logger);
 // }
 
-const middlewares = [logger];
-
 const persistConfig = {
   key: "root",
   // key: "views",
   storage,
-  // whitelist: ["views"],
+  // whitelist: ["searchFilterSlice"],
   blacklist: [
     "modalSlice",
     "categorySlice",
+    "buttonSlice",
     // "keywordConnectedSlice",
     // "searchFilterSlice",
   ],
@@ -34,27 +36,23 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userSlice,
+  buttonSlice,
   modalSlice,
   cardTypeSlice,
   categorySlice,
-
   subsSlice,
-
   keywordConnectedSlice,
   searchFilterSlice,
-
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false,
-//     }),
 const store = configureStore({
   // combineReducer와 동일하다
   reducer: persistedReducer,
-
-  middleware: [...middlewares],
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(logger),
 });
 
 export const persistor = persistStore(store);
