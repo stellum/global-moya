1b9f8faf5f1e13ea7f1a907cee423f43bbafbc80
youngcodej22 <youngcodej22@gmail.com>
2022-07-18 13:30:13 +0900

    feat: UPDATE QuickGuideMain
    delete test groupList component

--- client/src/pages/quickGuide/QuickGuideMain.jsx
+++ client/src/pages/quickGuide/QuickGuideMain.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import QuickGuideHeader from "./QuickGuideHeader";
 import QuickContent from "./QuickContent";
 import { DefaultContainer } from "@styles/common/container";
-import GroupList from "@components/group/GroupList";
 const QuickGuideMain = () => {
   return (
     <DefaultContainer>
       <QuickGuideHeader />
       <QuickContent />
-      <GroupList />
     </DefaultContainer>
   );
 };
