3ec3dfac2d5e5b093a560f2bed5da610a2c01def
oaat9309 <kite1993@naver.com>
2022-07-18 09:42:19 +0900

    feat: Edited Register Pages (07.18) 회원가입 페이지 구분 완료하였고, 약관 동의 부분 작업하였습니다.

--- client/src/pages/register/RegisterContainer.jsx
+++ client/src/pages/register/RegisterContainer.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Register from "./Register";
 import RegisterPolicy from "./RegisterPolicy";
+import RegisterSuccess from "./RegisterSuccess";
+import { ImageIcon } from "../../styles/register/registerPolicy";
+import BackArrow from "../../assets/images/BackArrow.svg";
 
 const RegisterContainer = () => {
+  const [registerPage, setRegisterPage] = useState("onPolicy");
+
   return (
     <div>
-      <RegisterPolicy />
-      <Register />
+      {/* backArrow 아이콘 들어갈 자리 */}
+      <h3>회원가입</h3>
+      {registerPage === "onPolicy" && (
+        <RegisterPolicy setRegisterPage={setRegisterPage} />
+      )}
+      {registerPage === "onInput" && (
+        <Register setRegisterPage={setRegisterPage} />
+      )}
+      {registerPage === "onSuccess" && <RegisterSuccess />}
     </div>
   );
 };
