dc6ca3101b374dc293bbf275842c966d929b482a
youngcodej22 <youngcodej22@gmail.com>
2022-07-18 13:19:13 +0900

    feat: UPDATE App.jsx
    add edit component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,8 @@ import PersonalPolicy from "@pages/myPage/PersonalPolicy";
 import ServicePolicy from "@pages/myPage/ServicePolicy";
 import Profile from "@pages/myPage/Profile";
 import Subscription from "./pages/myPage/Subscription";
+import EditKeywordContext from "@pages/edit/keyword/EditKeywordContext";
+// import EditGroupContext from "@pages/edit/group/EditGroupContext";
 
 function App() {
   return (
@@ -27,6 +29,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/main" element={<MainPage />} />
+            {/* Edit */}
+            <Route path="/main/edit/keyword" element={<EditKeywordContext />} />
+            {/* <Route path="/main/edit/group" element={<EditGroupContext />} /> */}
 
             {/* Register */}
             <Route path="/login" element={<Login />} />
