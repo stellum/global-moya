995c7aa09b3c2b76540a8d1ef81b3a5a49b999f0
youngcodej22 <youngcodej22@gmail.com>
2022-07-18 13:34:08 +0900

    feat: UPDATE searchFilterSlice
    add retry fucntion but it's not ready yet

--- client/src/redux/reducer/searchFilterSlice.js
+++ client/src/redux/reducer/searchFilterSlice.js
@@ -1,19 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getSearchData } from "@api/searchApi";
+import { retryAxios } from "@api/baseUrl";
+
 const initialState = {
   timeFilter: "mth1",
   mediaType: "mp,op,r",
   language: "en",
   orderBy: "latest",
-  keyType: "",
-  paramValue: "",
+  keyType: "category",
+  paramValue: "stocks",
+  status: "Welcome",
 };
 
-export const searchFilterSlice = createSlice({
-  name: "searchFilter",
+const fetchSearchNews = createAsyncThunk(
+  "searchFilterSlice/fetchSearchNews",
+  async (queryParams) => {
+    try {
+      console.log("slice param", queryParams);
+      const response = await getSearchData(queryParams);
+
+      if (response === undefined) {
+        retryAxios(3, 1000);
+      }
+
+      return response;
+    } catch (error) {
+      console.log("slice error", error);
+    }
+  }
+);
+
+const searchFilterSlice = createSlice({
+  name: "searchFilterSlice",
   initialState,
   reducers: {
     keywordContentRequest: (state, action) => {
-      console.log("action", action);
       state.keyType = action.payload[0];
       state.paramValue = action.payload[1];
     },
@@ -29,9 +50,21 @@ export const searchFilterSlice = createSlice({
     //   state.orderBy = action.payload;
     // }
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchSearchNews.pending, (state) => {
+      state.status = "Loading";
+    });
+    builder.addCase(fetchSearchNews.fulfilled, (state, action) => {
+      state.value = action.payload;
+      state.status = "complete";
+    });
+    builder.addCase(fetchSearchNews.rejected, (state) => {
+      state.status = "fail";
+    });
+  },
 });
 
+export default searchFilterSlice.reducer;
 export const { keywordContentRequest, loggedDefaultRequest } =
   searchFilterSlice.actions;
-
-export default searchFilterSlice.reducer;
+export { fetchSearchNews };
