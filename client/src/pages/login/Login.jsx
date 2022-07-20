6e1aa1b562938c052544a8bde06171f6dc357b15
oaat9309 <kite1993@naver.com>
2022-07-20 21:21:07 +0900

    feat: Edited Login Components (07.20) 로그인 api 에러 핸들링 일부 수정 후, 로그인 input 아이콘 상호 작용 추가했습니다.

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -69,6 +69,21 @@ const Login = () => {
     navigate("/register");
   };
 
+  // input 아이콘 동작용 state, ref
+
+  const [pwVisible, setPwVisible] = useState(false);
+
+  const eyeIconRef = useRef();
+
+  const handleShow = () => {
+    setPwVisible(pwVisible ? false : true);
+    let bgIcon = eyeIconRef.current.style.backgroundImage;
+    console.log(bgIcon);
+    pwVisible
+      ? (bgIcon = "client/src/assets/images/icons-eyeOpen.svg")
+      : (bgIcon = "client/src/assets/images/icons-eyeShut.svg");
+  };
+
   return (
     <>
       <CommonForm
@@ -98,6 +113,7 @@ const Login = () => {
 
         <InputDiv>
           <InputType
+            id="email"
             type="email"
             name="email"
             placeholder="이메일"
@@ -109,7 +125,12 @@ const Login = () => {
               },
             })}
           />
-          <IconCancel>
+          <IconCancel
+            onClick={() => {
+              const emailInput = document.querySelector("#email");
+              emailInput.value = "";
+            }}
+          >
             <IconText>icons-cancel</IconText>
           </IconCancel>
         </InputDiv>
@@ -122,7 +143,7 @@ const Login = () => {
               required: "비밀번호는 필수 입력입니다.",
             })}
           />
-          <ShowIcon>
+          <ShowIcon onClick={handleShow}>
             <IconText>눈동자 아이콘</IconText>
           </ShowIcon>
         </InputDiv>
