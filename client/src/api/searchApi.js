86d80c2bb6f15ae9685538dcaea3a3cfac2b4690
youngcodej22 <youngcodej22@gmail.com>
2022-07-21 23:54:33 +0900

    feat: UPDATE baseUrl, keywordList, search, login
    update response status and delete comment

--- client/src/api/searchApi.js
+++ client/src/api/searchApi.js
@@ -12,14 +12,6 @@ export const getSearchData = async (queryParams) => {
     nextPageToken,
   } = queryParams;
 
-  // axiosRetry(clientServer, { retries: 3 });
-  // clientServer
-  //   .get("/news/search") // The first request fails and the second returns 'ok'
-  //   .then((result) => {
-  //     console.log("result나다", result);
-  //     result.data; // 'ok'
-  //   });
-
   try {
     const response = await clientServer({
       url: "/news/search",
@@ -39,77 +31,19 @@ export const getSearchData = async (queryParams) => {
     if (response.status === 200) {
       const data = await response.data;
       return data;
-    }
-    if (response.status === 400) {
+    } else if (
+      response.status === 400 ||
+      response.data.code === 401 ||
+      response.data.code === 2002 ||
+      response.data.code === 4018
+    ) {
       const message = await response.data.message;
       return message;
-    }
-    if (response.data.code === 401) {
-      const message = await response.data.message;
-      return message;
-    }
-    if (response.data.code === 2002) {
-      const message = await response.data.message;
-      return message;
-    }
-    if (response.data.code === 4018) {
-      const message = await response.data.message;
-      return message;
+    } else {
+      console.log("etc message", response.data.message);
+      // return;
     }
   } catch (e) {
     console.log("error res", e);
-
-    // if (err.response.status !== 200) {
-    //   throw new Error(
-    //     `API call failed with status code: ${err.response.status} after 3 retry attempts`
-    //   );
-    // }
-
-    // if (e.config.url === "/news/search") {
-    //   console.log("에러여기");
-
-    //   const token = e.config.headers.Authorization.split(" ")[1];
-    //   getSearchData(e.config.params, token);
-    // }
-
-    // if (response === undefined) {
-    //   console.log("retry axios 실행");
-    //   retryAxios(3, 2000);
-    // }
-
-    // if (e.response.status === 400) {
-    return e.response;
-    // }
   }
 };
-
-/*
-axiosRetry(clientServer, {
-  retries: 3, // number of retries
-  retryDelay: (retryCount) => {
-    console.log(`retry attempt: ${retryCount}`);
-    return retryCount * 2000; // time interval between retries
-  },
-  retryCondition: (error) => {
-    console.log("retry errro", error);
-    // if retry condition is not specified, by default idempotent requests are retried
-    // if (error.config.data === undefined) {
-    //   const token = e.config.headers.Authorization.split(" ")[1];
-    //   getSearchData(e.config.params, token);
-    // }
-    // return error.response.status === 503;
-  },
-});
-
-// // Allows request-specific configuration
-// clientServer
-//   .get("/test", {
-//     "axios-retry": {
-//       retries: 0,
-//     },
-//   })
-//   .catch((error) => {
-//     // The first request fails
-//     error !== undefined;
-//   });
-*/
